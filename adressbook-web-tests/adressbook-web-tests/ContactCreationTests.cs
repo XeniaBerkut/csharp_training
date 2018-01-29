0f9d57cb8e4c70afa8bfaecf5b6b195487d6c1d5
XeniaBerkut <x.ivannikova@gmail.com>
2018-01-29 20:32:58 +0300

    Лекция 2.1. Общий базовый класс для тестов

--- adressbook-web-tests/adressbook-web-tests/ContactCreationTests.cs
+++ adressbook-web-tests/adressbook-web-tests/ContactCreationTests.cs
@@ -10,162 +10,20 @@ using OpenQA.Selenium.Support.UI;
 namespace WebAddressbookTests
 {
     [TestFixture]
-    public class ContactCreationTests
+    public class ContactCreationTests : TestBase
     {
-        private IWebDriver driver;
-        private StringBuilder verificationErrors;
-        private string baseURL;
-        private bool acceptNextAlert = true;
-
-        [SetUp]
-        public void SetupTest()
-        {
-            FirefoxOptions options = new FirefoxOptions();
-            options.BrowserExecutableLocation = @"c:\Program Files\Mozilla Firefox\firefox.exe";
-            options.UseLegacyImplementation = true;
-            driver = new FirefoxDriver(options);
-            baseURL = "http://localhost/";
-            verificationErrors = new StringBuilder();
-        }
-
-        [TearDown]
-        public void TeardownTest()
-        {
-            try
-            {
-                driver.Quit();
-            }
-            catch (Exception)
-            {
-                // Ignore errors if unable to close the browser
-            }
-            Assert.AreEqual("", verificationErrors.ToString());
-        }
+        
 
         [Test]
         public void ContactCreationTest()
         {
-            // общее, вынести в какой-то класс
-            OpenHomePage();
+            GoToHomePage();
             Login(new AccountData("admin", "secret"));
-            //end
-
             InitContactCreation();            
             ContactData contact = new ContactData("Hello", "Iam", "Xenia");
             FillContactForm(contact);
-            SubmitContactCreation();
+            SubmitCreation();
         }
 
-        private void SubmitContactCreation()
-        {
-            //SubmitContactCreation
-            driver.FindElement(By.Name("submit")).Click();
-        }
-
-        private void FillContactForm(ContactData contact)
-        {
-            // FillContactForm
-            driver.FindElement(By.Name("firstname")).Clear();
-            driver.FindElement(By.Name("firstname")).SendKeys(contact.Firstname);
-            driver.FindElement(By.Name("middlename")).Clear();
-            driver.FindElement(By.Name("middlename")).SendKeys(contact.Middlename);
-            driver.FindElement(By.Name("lastname")).Clear();
-            driver.FindElement(By.Name("lastname")).SendKeys(contact.Lastname);
-            //driver.FindElement(By.Name("nickname")).Clear();
-            //driver.FindElement(By.Name("nickname")).SendKeys("2");
-            //driver.FindElement(By.Name("title")).Clear();
-            //driver.FindElement(By.Name("title")).SendKeys("3");
-            //driver.FindElement(By.Name("company")).Clear();
-            //driver.FindElement(By.Name("company")).SendKeys("4");
-            //driver.FindElement(By.Name("address")).Clear();
-            //driver.FindElement(By.Name("address")).SendKeys("вафывафывафываф");
-            //driver.FindElement(By.Name("home")).Clear();
-            //driver.FindElement(By.Name("home")).SendKeys("дом");
-            //driver.FindElement(By.Name("mobile")).Clear();
-            //driver.FindElement(By.Name("mobile")).SendKeys("мобилка");
-            //driver.FindElement(By.Name("work")).Clear();
-            //driver.FindElement(By.Name("work")).SendKeys("работа");
-            //driver.FindElement(By.Name("fax")).Clear();
-            //driver.FindElement(By.Name("fax")).SendKeys("факс");
-            //driver.FindElement(By.Name("email")).Clear();
-            //driver.FindElement(By.Name("email")).SendKeys("мыло");
-            //driver.FindElement(By.Name("email2")).Clear();
-            //driver.FindElement(By.Name("email2")).SendKeys("мыло");
-            //driver.FindElement(By.Name("email3")).Clear();
-            //driver.FindElement(By.Name("email3")).SendKeys("мыло");
-            //driver.FindElement(By.Name("homepage")).Clear();
-            //driver.FindElement(By.Name("homepage")).SendKeys("дом");
-            //new SelectElement(driver.FindElement(By.Name("bday"))).SelectByText("1");
-            //new SelectElement(driver.FindElement(By.Name("bmonth"))).SelectByText("January");
-            //driver.FindElement(By.Name("byear")).Clear();
-            //driver.FindElement(By.Name("byear")).SendKeys("1233");
-        }
-
-        private void InitContactCreation()
-        {
-            //InitContactCreation
-            driver.FindElement(By.LinkText("add new")).Click();
-        }
-
-        private void Login(AccountData account)
-        {
-            driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys(account.Username);
-            driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
-            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
-        }
-
-        private void OpenHomePage()
-        {
-            driver.Navigate().GoToUrl(baseURL + "addressbook/");
-        }
-        private bool IsElementPresent(By by)
-        {
-            try
-            {
-                driver.FindElement(by);
-                return true;
-            }
-            catch (NoSuchElementException)
-            {
-                return false;
-            }
-        }
-
-        private bool IsAlertPresent()
-        {
-            try
-            {
-                driver.SwitchTo().Alert();
-                return true;
-            }
-            catch (NoAlertPresentException)
-            {
-                return false;
-            }
-        }
-
-        private string CloseAlertAndGetItsText()
-        {
-            try
-            {
-                IAlert alert = driver.SwitchTo().Alert();
-                string alertText = alert.Text;
-                if (acceptNextAlert)
-                {
-                    alert.Accept();
-                }
-                else
-                {
-                    alert.Dismiss();
-                }
-                return alertText;
-            }
-            finally
-            {
-                acceptNextAlert = true;
-            }
-        }
-    }
+     }
 }
---
+++ adressbook-web-tests/adressbook-web-tests/TestBase.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebAddressbookTests
+{
+    public class TestBase
+    {
+        protected IWebDriver driver;
+        private StringBuilder verificationErrors;
+        protected string baseURL;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            FirefoxOptions options = new FirefoxOptions();
+            options.BrowserExecutableLocation = @"c:\Program Files\Mozilla Firefox\firefox.exe";
+            options.UseLegacyImplementation = true;
+            driver = new FirefoxDriver(options);
+            baseURL = "http://localhost/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        protected void GoToHomePage()
+        {
+            driver.Navigate().GoToUrl(baseURL + "addressbook/");
+        }
+
+        protected void Login(AccountData account)
+        {
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(account.Username);
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
+            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
+        }
+
+        protected void GoToGroupsPage()
+        {
+            driver.FindElement(By.LinkText("groups")).Click();
+        }
+
+        protected void InitContactCreation()
+        {
+            driver.FindElement(By.LinkText("add new")).Click();
+        }
+
+        protected void SelectGroup(int index)
+        {
+            driver.FindElement(By.XPath("(//input[@name='selected[]'])[" + index + "]")).Click();
+        }
+
+
+        protected void FillGroupForm(GroupData group)
+        {
+            driver.FindElement(By.Name("group_name")).Clear();
+            driver.FindElement(By.Name("group_name")).SendKeys(group.Name);
+            driver.FindElement(By.Name("group_header")).Clear();
+            driver.FindElement(By.Name("group_header")).SendKeys(group.Header);
+            driver.FindElement(By.Name("group_footer")).Clear();
+            driver.FindElement(By.Name("group_footer")).SendKeys(group.Footer);
+        }
+
+        protected void RemoveGroup()
+        {
+            driver.FindElement(By.Name("delete")).Click();
+        }
+
+        protected void ReturnToGroupsPage()
+        {
+            driver.FindElement(By.LinkText("group page")).Click();
+        }
+
+        protected void InitGroupCreation()
+        {
+            driver.FindElement(By.Name("new")).Click();
+        }
+
+        protected void SubmitCreation()
+        {
+            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        protected void Logout()
+        {
+            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+
+        //private void SubmitContactCreation()
+        //{
+        //    driver.FindElement(By.Name("submit")).Click();
+        //}
+
+        protected void FillContactForm(ContactData contact)
+        {
+            driver.FindElement(By.Name("firstname")).Clear();
+            driver.FindElement(By.Name("firstname")).SendKeys(contact.Firstname);
+            driver.FindElement(By.Name("middlename")).Clear();
+            driver.FindElement(By.Name("middlename")).SendKeys(contact.Middlename);
+            driver.FindElement(By.Name("lastname")).Clear();
+            driver.FindElement(By.Name("lastname")).SendKeys(contact.Lastname);
+            //driver.FindElement(By.Name("nickname")).Clear();
+            //driver.FindElement(By.Name("nickname")).SendKeys("2");
+            //driver.FindElement(By.Name("title")).Clear();
+            //driver.FindElement(By.Name("title")).SendKeys("3");
+            //driver.FindElement(By.Name("company")).Clear();
+            //driver.FindElement(By.Name("company")).SendKeys("4");
+            //driver.FindElement(By.Name("address")).Clear();
+            //driver.FindElement(By.Name("address")).SendKeys("вафывафывафываф");
+            //driver.FindElement(By.Name("home")).Clear();
+            //driver.FindElement(By.Name("home")).SendKeys("дом");
+            //driver.FindElement(By.Name("mobile")).Clear();
+            //driver.FindElement(By.Name("mobile")).SendKeys("мобилка");
+            //driver.FindElement(By.Name("work")).Clear();
+            //driver.FindElement(By.Name("work")).SendKeys("работа");
+            //driver.FindElement(By.Name("fax")).Clear();
+            //driver.FindElement(By.Name("fax")).SendKeys("факс");
+            //driver.FindElement(By.Name("email")).Clear();
+            //driver.FindElement(By.Name("email")).SendKeys("мыло");
+            //driver.FindElement(By.Name("email2")).Clear();
+            //driver.FindElement(By.Name("email2")).SendKeys("мыло");
+            //driver.FindElement(By.Name("email3")).Clear();
+            //driver.FindElement(By.Name("email3")).SendKeys("мыло");
+            //driver.FindElement(By.Name("homepage")).Clear();
+            //driver.FindElement(By.Name("homepage")).SendKeys("дом");
+            //new SelectElement(driver.FindElement(By.Name("bday"))).SelectByText("1");
+            //new SelectElement(driver.FindElement(By.Name("bmonth"))).SelectByText("January");
+            //driver.FindElement(By.Name("byear")).Clear();
+            //driver.FindElement(By.Name("byear")).SendKeys("1233");
+        }
+    }
+}
