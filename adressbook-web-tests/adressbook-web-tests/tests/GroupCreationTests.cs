8465aad95803c32516de8fb4ee6a3d1a6a1f6b31
XeniaBerkut <x.ivannikova@gmail.com>
2018-02-13 21:19:27 +0300

    Лекция 4.1. Коллекции (списки)

--- adressbook-web-tests/adressbook-web-tests/tests/GroupCreationTests.cs
+++ adressbook-web-tests/adressbook-web-tests/tests/GroupCreationTests.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace WebAddressbookTests
@@ -16,8 +17,12 @@ namespace WebAddressbookTests
             group.Header = "world";
             group.Footer = "!";
 
-            
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
             app.Groups.Create(group);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            Assert.AreEqual(oldGroups.Count + 1, newGroups.Count);
             
         }
 
@@ -28,8 +33,25 @@ namespace WebAddressbookTests
             group.Header = "";
             group.Footer = "";
 
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
             app.Groups.Create(group);
-            
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            Assert.AreEqual(oldGroups.Count + 1, newGroups.Count);
+
+        }
+
+        [Test]
+        public void BadNameGroupCreationTest()
+        {
+            GroupData group = new GroupData("a ' a");
+            group.Header = "";
+            group.Footer = "";
+
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            app.Groups.Create(group);
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            Assert.AreEqual(oldGroups.Count + 1, newGroups.Count);
+
         }
 
 
---
+++ adressbook-web-tests/adressbook-web-tests/tests/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace adressbook_web_tests.tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string[] s = new string[] { "test", "a", "+", "b" };
+            for (int i = 0; i < s.Length; i++)
+            {
+                System.Console.Out.Write(s[i] + "\n");
+            }
+                       
+
+            foreach (string element in s)
+            {
+                System.Console.Out.Write(element + "\n");
+            }
+
+            IWebDriver driver = null;
+            int attempt = 0;
+
+            while (driver.FindElements(By.Id("test")).Count == 0 && attempt < 60)
+            {
+                System.Threading.Thread.Sleep(1000);
+                attempt++;
+            }
+            //...
+
+            do
+            {
+                System.Threading.Thread.Sleep(1000);
+                attempt++;
+            } while (driver.FindElements(By.Id("test")).Count == 0 && attempt < 60);
+
+
+        }
+    }
+}
