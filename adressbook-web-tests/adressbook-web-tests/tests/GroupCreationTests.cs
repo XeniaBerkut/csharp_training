a3eb81145c27a1c78fa8545a72a072e17c9827aa
XeniaBerkut <x.ivannikova@gmail.com>
2018-02-26 21:14:36 +0300

    Лекция 5.5. Параметризация тестов, Data Driven Testing

--- adressbook-web-tests/adressbook-web-tests/tests/GroupCreationTests.cs
+++ adressbook-web-tests/adressbook-web-tests/tests/GroupCreationTests.cs
@@ -10,13 +10,27 @@ namespace WebAddressbookTests
     [TestFixture]
     public class GroupCreationTests : AuthTestBase
     {
-        [Test]
-        public void GroupCreationTest()
+        public static IEnumerable<GroupData> RandomGroupDataProvider()
         {
-            GroupData group = new GroupData("Hello");
-            group.Header = "world";
-            group.Footer = "!";
+            List<GroupData> groups = new List<GroupData>();
+            for (int i = 0; i < 5; i++)
+            {
+                groups.Add(new GroupData(GenerateRandomString(30))
+                {
+                    Header = GenerateRandomString(100),
+                    Footer = GenerateRandomString(100)
+                });
+                {
 
+                }
+            }
+            return groups;
+        }
+
+
+        [Test, TestCaseSource("RandomGroupDataProvider")]
+        public void GroupCreationTest(GroupData group)
+        {
             List<GroupData> oldGroups = app.Groups.GetGroupList();
 
             app.Groups.Create(group);
@@ -31,7 +45,7 @@ namespace WebAddressbookTests
 
         }
 
-        [Test]
+       /* [Test]
         public void EmptyGroupCreationTest()
         {
             GroupData group = new GroupData("");
@@ -51,7 +65,7 @@ namespace WebAddressbookTests
             Assert.AreEqual(oldGroups, newGroups);
         }
 
-        /* [Test]
+         [Test]
          public void BadNameGroupCreationTest()
          {
              GroupData group = new GroupData("a ' a");
